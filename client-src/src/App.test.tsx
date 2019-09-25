3cb4dab76cdd45eebde9b9b3d72c1ca0e97264bd
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-09-25 16:28:52 -0700

    Improve auth tests further

--- client-src/src/App.test.tsx
+++ client-src/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import {App} from "./App";
+import wait from 'waait';
 
 it('renders without crashing', () => {
     expect(() => render(<App/>)).not.toThrow();
@@ -11,14 +12,31 @@ it('renders the home page without logging in', async () => {
   expect(await findByText('Hi, y\'all')).toBeInTheDocument();
 });
 
-it('handles the login page properly', async () => {
-  const {findByText, findByLabelText, debug} = render(<App/>);
+it('handles the login page errors properly', async () => {
+  const {findByText, findByLabelText} = render(<App/>);
   fireEvent.click(await findByText('Login'));
   expect(window.location.pathname).toBe('/login');
+
+  // Reset button should be disabled until there is input
   expect(await findByText('Reset')).toBeDisabled();
-  fireEvent.change(await findByLabelText('Password'), {target: {value: 'test'}});
-  fireEvent.change(await findByLabelText('Email'), {target: {value: 'test@example.com'}});
+
+  const passwordInput = await findByLabelText('Password');
+  const emailInput = await findByLabelText('Email');
+
+  // Empty fields should show an error
+  fireEvent.click(await findByText('Submit'));
+
+  // Wait for the form to validate
+  await wait(1);
+
+  expect(passwordInput).toHaveClass('error');
+  expect(emailInput).toHaveClass('error');
+
+  // Let's apply text
+  fireEvent.change(passwordInput, {target: {value: 'test'}});
+  fireEvent.change(emailInput, {target: {value: 'test@example.com'}});
+  // Once there is input, undo the reset button disable state
   expect(await findByText('Reset')).not.toBeDisabled();
   fireEvent.click(await findByText('Submit'));
-  debug();
+  await wait(1);
 });
